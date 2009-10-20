1cee35222104cf0d56a8dd9570f9698a0785979a
Arvid Paeglit <arvid@arvid-desktop.(none)>
2009-10-20 17:21:57 +0300

    changed media linking

--- user_site/settings.py
+++ user_site/settings.py
@@ -1,14 +1,6 @@
 from saaskit.settings import *
 
-import os.path
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
 SITE_ID = 2
-
-# Absolute path to the directory that holds media.
-# Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
-APP_MEDIA_ROOT = MEDIA_ROOT
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = '+*q3$z(d1@hi^p%645&636$n7r@=w!m)(z9@k9&9s9_7uh%a+s'
