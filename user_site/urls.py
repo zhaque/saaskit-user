58ba9e2771f577ec4c1b79bef36f792eda7f63b3
Arvid Paeglit <arvid@arvid-desktop.(none)>
2009-10-31 11:54:21 +0200

    'manage content' url mapping

--- user_site/urls.py
+++ user_site/urls.py
@@ -5,10 +5,21 @@ from django.views.generic import simple
 from django.views.generic.simple import direct_to_template
 handler404 = 'perfect404.views.page_not_found'
 
+import frontendadmin.views
 from django_authopenid import views as oid_views
 from registration import views as reg_views
 
 from muaccounts.views.decorators import public
+from muaccount_content.forms import MuFlatpageAddForm, MuFlatpageChangeForm
+
+def mu_initial(func):
+    def wrapped(request, initial=None, *args, **kwargs):
+        if initial is None: initial = {}
+        initial['muaccount'] = request.muaccount.id
+        return func(request, initial=initial, *args, **kwargs)
+    
+    return wrapped
+
 
 urlpatterns = patterns('',
     url(r'^$', 'django.views.generic.simple.direct_to_template', {
@@ -24,10 +35,6 @@ urlpatterns = patterns('',
     url(r'^admin/dashboard/$', views.notices, {
         'template': 'account/dashboard.html',
     }, name='account_dashboard'),
-
-    url(r'^extend/apps/$', simple.direct_to_template, {
-        'template': 'manage_apps.html',
-    }),
 
     url(r'^extend/plans/$', simple.direct_to_template, {
         'template': 'account_plans.html',
@@ -54,6 +61,31 @@ urlpatterns = patterns('',
     (r'^profiles/', include('saaskit_profile.urls')),
     (r'^notices/', include('notification.urls')),
     (r'^subscription/', include('subscription.urls')),
+    (r'^tinymce/', include('tinymce.urls')),
+    
+    #Manage content
+    url(r'^admin/apps/$', 'muaccount_content.views.mu_listing', name='app_settings'),
+    
+    url(r'^content/add/(?P<app_label>muaccount_content)/(?P<model_name>muflatpage)/$', 
+        mu_initial(frontendadmin.views.add),
+        {'form_exclude': ('enable_comments', 'template_name', 'use_default', 'active'),
+         'form_class': MuFlatpageAddForm,
+         'initial': {'sites': [settings.SITE_ID,]},
+         'template_name': 'manage/frontendadmin_form.html',
+         },
+        name='frontendadmin_add'
+    ),
+    
+    url(r'^content/change/(?P<app_label>muaccount_content)/(?P<model_name>muflatpage)/(?P<instance_id>[\d]+)/$', 
+        mu_initial(frontendadmin.views.change),
+        {'form_exclude': ('enable_comments', 'template_name'),
+         'form_class': MuFlatpageChangeForm,
+         'template_name': 'manage/frontendadmin_form.html',
+         },
+        name='frontendadmin_change'
+    ),
+    
+    (r'^content/', include('frontendadmin.urls')),
 )
 
 # serve static files in debug mode
