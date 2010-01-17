a0dcdd51b8246799088cbaff9a8e9d7861bc5cdf
saaskit.org <admin@saaskit.org>
2010-01-17 03:20:04 -0700

    package and repo name should be same.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-setup(name="saaskit-user-site",
+setup(name="saaskit-user",
            version="0.1",
            description="Django project for user sites of saaskit",
            author="SaaSkit",
